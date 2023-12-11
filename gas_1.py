fe85d99fea3dedaeedb76e9d2ba965424ce7e405
StarSSeeuS <igorolam21@gmail.com>
2023-12-12 01:15:17 +0300

    %записывает данные в файл

--- gas_1.py
+++ gas_1.py
@@ -1,6 +1,5 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+import pandas as pd
 
 # Constants
 epsilon = 1.0
@@ -8,25 +7,32 @@ sigma = 1.0
 mass = 1.0
 dt = 0.01  # Time step
 num_steps = 1000
+box_size = 10.0  # Size of the simulation box
 
 # Function to calculate LJ potential and force
-def lj_potential(r):
-    return 4 * epsilon * ((sigma / r)**12 - (sigma / r)**6)
-
 def lj_force(r):
     return 24 * epsilon / r * (2 * (sigma / r)**12 - (sigma / r)**6)
 
-# Function to perform Verlet integration
-def verlet_integration(positions, velocities, dt):
+# Function to apply periodic boundary conditions
+def apply_periodic_boundary_conditions(positions, box_size):
+    return np.mod(positions, box_size)
+
+# Function to perform Verlet integration with periodic boundary conditions
+def verlet_integration_periodic(positions, velocities, dt, box_size):
     num_particles = len(positions)
     forces = np.zeros_like(positions)
+
+    # Apply periodic boundary conditions
+    positions = apply_periodic_boundary_conditions(positions, box_size)
 
     # Calculate forces
     for i in range(num_particles):
         for j in range(i + 1, num_particles):
-            r = np.linalg.norm(positions[i] - positions[j])
-            force_ij = lj_force(r)
-            direction_ij = (positions[j] - positions[i]) / r
+            r = positions[j] - positions[i]
+            r = r - np.round(r / box_size) * box_size  # Account for PBC
+            distance = np.linalg.norm(r)
+            force_ij = lj_force(distance)
+            direction_ij = r / distance
             forces[i] += force_ij * direction_ij
             forces[j] -= force_ij * direction_ij
 
@@ -35,28 +41,47 @@ def verlet_integration(positions, velocities, dt):
     new_forces = np.zeros_like(positions)
     for i in range(num_particles):
         for j in range(i + 1, num_particles):
-            r = np.linalg.norm(positions[i] - positions[j])
-            force_ij = lj_force(r)
-            direction_ij = (positions[j] - positions[i]) / r
+            r = positions[j] - positions[i]
+            r = r - np.round(r / box_size) * box_size  # Account for PBC
+            distance = np.linalg.norm(r)
+            force_ij = lj_force(distance)
+            direction_ij = r / distance
             new_forces[i] += force_ij * direction_ij
             new_forces[j] -= force_ij * direction_ij
     velocities += 0.5 * (forces + new_forces) / mass * dt
+
+    # Apply periodic boundary conditions again
+    positions = apply_periodic_boundary_conditions(positions, box_size)
 
     return positions, velocities
 
 # Simulation
 num_particles = 10
-positions = np.random.rand(num_particles, 3)
+positions = np.random.rand(num_particles, 3) * box_size
 velocities = np.random.rand(num_particles, 3)
 
-for step in range(num_steps):
-    positions, velocities = verlet_integration(positions, velocities, dt)
+# Arrays to store kinetic energy and temperature over time
+kinetic_energy = np.zeros(num_steps)
+temperature = np.zeros(num_steps)
 
-# Visualization (3D plot)
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.scatter(positions[:, 0], positions[:, 1], positions[:, 2])
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
-plt.show()
+for step in range(num_steps):
+    positions, velocities = verlet_integration_periodic(positions, velocities, dt, box_size)
+    
+    # Calculate kinetic energy and temperature
+    kinetic_energy[step] = 0.5 * mass * np.sum(velocities**2)
+    temperature[step] = 2 * kinetic_energy[step] / (3 * num_particles)  # Equipartition theorem
+
+# Print average kinetic energy and temperature
+average_kinetic_energy = np.mean(kinetic_energy)
+average_temperature = np.mean(temperature)
+
+# Write data to a csv file
+data = {
+    'Kinetic Energy': kinetic_energy,
+    'Temperature': temperature
+}
+df = pd.DataFrame(data)
+df.to_csv('simulation_data.csv', index=False)
+
+print(f"Average Kinetic Energy: {average_kinetic_energy}")
+print(f"Average Temperature: {average_temperature}")
