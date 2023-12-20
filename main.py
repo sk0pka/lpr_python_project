cad5a913189dcf110fc87d974ddffd6962d6b50f
sk0pka <m.skopkareva@gmail.com>
2023-12-21 00:30:45 +0300

    upd maim

--- main.py
+++ main.py
@@ -2,11 +2,7 @@ import math
 import numpy as np
 import time
 
-<<<<<<< HEAD
 from particleclass import Particle
-=======
-from particles import Particle
->>>>>>> 379679bd109159e0af1cf0e1b677b7e4acd801d1
 
 # глобальные переменные
 N = int(27)  # количество частиц
@@ -76,7 +72,6 @@ def calc_axel(particles):
     for i in np.arange(N-1):
         for j in np.arange(i+1, N):
             axel(particles[i], particles[j])
-<<<<<<< HEAD
 
 
 def first_move(particles):
@@ -162,92 +157,6 @@ def display_coordinates(particles):
         coord.write('\n')
 
 
-=======
-
-
-def first_move(particles):
-    # moves all particles for the first time
-    calc_axel(particles)
-    for i in np.arange(N):
-        Particle.first_move(particles[i])
-
-
-def move(particles):
-    # moves all particles
-    calc_axel(particles)
-    for i in np.arange(N):
-        Particle.move(particles[i])
-
-
-def potentwo(part, part1):
-    # calculates the potential energy of the interaction of two particles
-    vect_r = Particle.vec_to_virtual_copy(part.c, part1.c)
-    abs_r = np.linalg.norm(vect_r)
-    u = 4*(np.power(abs_r, -12) - np.power(abs_r, -6))
-    return u
-
-
-def impulse(particles):
-    # calculates the total momentum of the system
-    summ = np.zeros(3)
-    for i in np.arange(N):
-        summ += particles[i].v
-    impt.write(np.array2string(summ) + '\n')
-
-
-def poten_eng(particles):
-    # calculates the potential energy of the interaction of all particles
-    pot = 0.0
-    for i in np.arange(N-1):
-        for j in np.arange(i+1, N):
-            pot += potentwo(particles[i], particles[j])
-    pott.write(str(pot) + '\n')
-    return pot
-
-
-def kinetic_eng(particles):
-    # calculates the total kinetic energy of the system
-    kin = 0.0
-    for i in np.arange(N):
-        kin += (np.linalg.norm(particles[i].v)**2)/2
-    kint.write(str(kin) + '\n')
-    return kin
-
-
-def energy(particles):
-    # calculates the total mechanical energy of the system
-    pot = poten_eng(particles)
-    kin = kinetic_eng(particles)
-    summ = pot + kin
-    mect.write(str(summ) + '\n')
-
-
-def maxwellx(particles):
-    list = np.zeros(N)
-    for i in np.arange(N):
-        list[i] = particles[i].v[0]
-    list = np.sort(list)
-    for i in np.arange(N):
-        maxwt.write(str(list[i]) + '\n')
-
-
-def average_way(particles):
-    summ = 0.0
-    for i in np.arange(N):
-        summ += (np.linalg.norm(particles[i].way))**2
-    summ = summ/N
-    wayt.write(str(summ) + '\n')
-
-def display_coordinates(particles):
-    coord.write(str(N) + '\n')
-    coord.write('Lattice="10.0 0.0 0.0 0.0 10.0 0.0 0.0 0.0 10.0" Properties=S:1:pos:R:3' + '\n')
-    for i in np.arange(N):
-        for j in np.arange(3):
-            coord.write(str(particles[i].c[j]) + ' ')
-        coord.write('\n')
-
-
->>>>>>> 379679bd109159e0af1cf0e1b677b7e4acd801d1
 def timego(particles, tick):
     "starts the simulation"
     # display_coordinates(particles)
@@ -268,7 +177,6 @@ def timego(particles, tick):
     maxwellx(particles)
     print(100, '%')
 
-<<<<<<< HEAD
 def main():
     t = int(50000)  # ticks
     start = time.time()  # точка отсчета времени
@@ -291,28 +199,3 @@ pott.close()
 maxwt.close()
 wayt.close()
 coord.close()
-=======
-
-    def main():
-        t = int(50000)  # ticks
-        start = time.time()  # точка отсчета времени
-        particles = [] # particle array
-        cell_gen(particles)  # генерация сеткой  
-        timego(particles, t)
-        end = time.time() - start  # собственно время работы программы
-        print(end)  # вывод времени
-
-
-    if name == "main":
-        main()
-
-
-    # close files with data
-    impt.close()
-    kint.close()
-    mect.close()
-    pott.close()
-    maxwt.close()
-    wayt.close()
-    coord.close()
->>>>>>> 379679bd109159e0af1cf0e1b677b7e4acd801d1
