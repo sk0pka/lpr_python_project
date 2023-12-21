5d0b1812e0d3662e986655a35111795f15070d54
sk0pka <mary-al-sko@yandex.ru>
2023-12-21 14:13:01 +0300

    pep8energy

--- energy.py
+++ energy.py
@@ -1,34 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-ymec = np.array([])
-f = open('mec.txt')
-for line in f:
-    ymec = np.append(ymec, (float(line)))
-f.close()
+# Read data from files
+def read_data(filename):
+    with open(filename, 'r') as f:
+        return np.array([float(line.strip()) for line in f])
 
-ypot = np.array([])
-f = open('pot.txt')
-for line in f:
-    ypot = np.append(ypot, (float(line)))
-f.close()
-
-ykin = np.array([])
-f = open('kin.txt')
-for line in f:
-    ykin = np.append(ykin, (float(line)))
-f.close()
+ymec = read_data('mec.txt')
+ypot = read_data('pot.txt')
+ykin = read_data('kin.txt')
 
 x = np.linspace(0, len(ymec), num=len(ymec))
 
-
+# Plotting
 plt.xlabel(r'Время работы программы, тиков', fontsize=14)
 plt.ylabel(r'Энергия, у.е.', fontsize=14)
 plt.title(r'График зависимости разных видов энергии от времени', fontsize=14)
 plt.grid(True)
+
 plt.errorbar(x, ymec, fmt='o', color='black', capsize=3, label=r'Полная механическая энергия')
-plt.errorbar(x, ykin, fmt='o', color='red', capsize=3, label=r'Кинетическая энергия частиц')
-plt.errorbar(x, ypot, fmt='o', color='green', capsize=3, label=r'Потенциальная энергия частиц')
+plt.errorbar(x, ykin, fmt='o', color='orange', capsize=3, label=r'Кинетическая энергия частиц')
+plt.errorbar(x, ypot, fmt='o', color='blue', capsize=3, label=r'Потенциальная энергия частиц')
+
 plt.legend(loc='best', fontsize=12)
+plt.tight_layout()
 
 plt.show()
